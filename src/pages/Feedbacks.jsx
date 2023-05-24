a8b973a6db0ce9615496d4e492ddff283ba28a9c
Geovanna Otoni <geovannaotoni@gmail.com>
2023-05-24 08:24:08 -0300

    feat14: exibe o score e assertions na pagina de feedback

--- src/pages/Feedbacks.jsx
+++ src/pages/Feedbacks.jsx
@@ -15,10 +15,15 @@ class Feedbacks extends Component {
   }
 
   render() {
+    const { assertions, score } = this.props;
     return (
       <div>
         <Header />
         <p>Feedback</p>
+        <p>Your Score</p>
+        <span data-testid="feedback-total-score">{ score }</span>
+        <p>Your Assertions</p>
+        <span data-testid="feedback-total-question">{ assertions }</span>
         <div data-testid="feedback-text">{this.feedbackMessage()}</div>
       </div>
     );
@@ -27,10 +32,12 @@ class Feedbacks extends Component {
 
 Feedbacks.propTypes = {
   assertions: PropTypes.number,
+  score: PropTypes.number,
 }.isRequired;
 
 const mapStateToProps = (globalState) => ({
   assertions: globalState.player.assertions,
+  score: globalState.player.score,
 });
 
 export default connect(mapStateToProps)(Feedbacks);
