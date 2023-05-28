cde140a67f446097e893822aefdd3d0cddc28308
Geovanna Otoni <geovannaotoni@gmail.com>
2023-05-28 12:31:11 -0300

    feat19: adiciona as informações do jogador no localStorage e traz a lista ordenada no ranking

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,9 +2,24 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import md5 from 'crypto-js/md5';
 import Header from '../components/header';
+import { setPlayerOnStorage } from '../services/localStorage';
+import { addScore } from '../redux/actions';
 
 class Feedback extends Component {
+  savePlayerOnStorage = () => {
+    const { name, score, email, history, dispatch } = this.props;
+    const hash = md5(email).toString();
+    setPlayerOnStorage({
+      name,
+      score,
+      gravatarImg: hash,
+    });
+    dispatch(addScore(0, 0)); // resetar o jogo
+    history.push('/ranking');
+  };
+
   feedbackMessage() {
     const { assertions } = this.props;
     const hit = 3;
@@ -29,15 +44,13 @@ class Feedback extends Component {
         <Link to="/" data-testid="btn-play-again">
           <button type="button">Play Again</button>
         </Link>
-        <Link to="/ranking">
-          <button
-            type="button"
-            data-testid="btn-ranking"
-          >
-            Ranking
-
-          </button>
-        </Link>
+        <button
+          type="button"
+          data-testid="btn-ranking"
+          onClick={ this.savePlayerOnStorage }
+        >
+          Ranking
+        </button>
       </div>
     );
   }
@@ -46,11 +59,19 @@ class Feedback extends Component {
 Feedback.propTypes = {
   assertions: PropTypes.number,
   score: PropTypes.number,
+  email: PropTypes.string,
+  name: PropTypes.string,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  dispatch: PropTypes.func,
 }.isRequired;
 
 const mapStateToProps = (globalState) => ({
   assertions: globalState.player.assertions,
   score: globalState.player.score,
+  email: globalState.userReducer.email,
+  name: globalState.userReducer.name,
 });
 
 export default connect(mapStateToProps)(Feedback);
--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -3,3 +3,10 @@ export const setTokenOnStorage = (token) => {
 };
 
 export const getTokenFromStorage = () => (localStorage.getItem('token'));
+
+export const getPlayersFromStorage = () => JSON.parse(localStorage.getItem('players'));
+
+export const setPlayerOnStorage = (player) => {
+  const players = getPlayersFromStorage() || [];
+  localStorage.setItem('players', JSON.stringify([...players, player]));
+};
